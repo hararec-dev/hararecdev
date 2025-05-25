42cbf993ae50017f58ac3fd40c7cf50db45e8520
hararec-dev <hararecdev@gmail.com>
2025-05-24 18:33:14 -0600

    style(ui): update color schemes and add new SVG assets

--- src/components/shared/GradientText.tsx
+++ src/components/shared/GradientText.tsx
@@ -4,7 +4,7 @@ export type GradientTextProps = React.PropsWithChildren< {
 
 export const GradientText: React.FC<GradientTextProps> = ({ children, className = "" }) => {
     return (
-        <span className={`bg-clip-text text-transparent bg-gradient-to-r from-indigo-600 via-fuchsia-600 to-pink-600 dark:from-sky-400 dark:via-violet-500 dark:to-pink-500 ${className}`}>
+        <span className={`bg-clip-text text-transparent bg-gradient-to-r from-sky-600 via-indigo-500 to-purple-600 dark:from-sky-400 dark:via-violet-500 dark:to-pink-500 ${className}`}>
             {children}
         </span>
     );
